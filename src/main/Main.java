ee0ea49967f685e2557be3428da74a23d1915b1a
Varuncodes07 <varunkumar10107@gmail.com>
2023-05-25 10:08:24 -0400

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,6 +11,8 @@ public class Main {
 		
 		System.out.println(animals.get(0) instanceof Manatee);
 		System.out.println(animals.get(0) instanceof Animal);
+
+		System.out.println("pofdsfo")
 	}
 
 }
