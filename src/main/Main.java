8dd54615e3dc50c3235dd5d0010bff9f3165937c
BigDaddy <105324939+polatemakaleem@users.noreply.github.com>
2023-05-24 21:57:10 -0400

    Example Push

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,8 +9,9 @@ public class Main {
 		
 		animals.add(new Manatee("Bob", 1));
 		
-		System.out.println(animals.get(0) instanceof Manatee);
-		System.out.println(animals.get(0) instanceof Animal);
+		System.out.println("Class Manatee: " + (animals.get(0) instanceof Manatee));
+		System.out.println("Class Animal: " + (animals.get(0) instanceof Animal));
+		System.out.println(animals.toString());
 	}
 
 }
