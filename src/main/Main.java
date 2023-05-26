d21172f9d3cfb9cd2885ad75a9c7923ed3083a67
BigDaddy <105324939+polatemakaleem@users.noreply.github.com>
2023-05-26 18:21:36 -0400

    blargh
    - added menu to choose which animal species each animal is
    
    - able to remove animals from map

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,8 +6,9 @@ public class Main {
 	public static int getInt(String question, Scanner scanner) {
 		while(true) {
 			System.out.print(question);
+			String response = scanner.nextLine();
 			try {
-				return scanner.nextInt();
+				return Integer.parseInt(response);
 			} catch(Exception e) {
 				System.out.println("Please enter an integer.\n");
 			}
@@ -22,29 +23,55 @@ public class Main {
 		}
 	}
 	
+	public static Animal animalChoice(int option, Scanner scanner) {
+		System.out.print("\nEnter name of animal: ");
+		String name = scanner.nextLine();
+		switch(option) {
+			case 1 : {
+				return new Manatee(name);
+			}
+			
+			default : {
+				return null;
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Aquarium aquarium = new Aquarium();
 		while(true) {
-			System.out.println("\n\t\tAQUARIUM\n\n\t1. Add Animal\n\t2. Print Animal Data\n\t3. Exit");
+			System.out.println("\n\t\tAQUARIUM\n\n\t1. Add Animal\n\t2. Remove Animal\n\t3. Print Animal Data");
 			int option = getInt("\nEnter option: ", scanner);
 			
 			switch(option) {
 				case 1 : {
-					System.out.print("\nEnter name of animal: ");
-					aquarium.addAnimal(new Animal(scanner.next()), generateId(aquarium));
+					
+					while(true) {
+						System.out.println("\n\n\t\t\t\tANIMALS:\n\n\t1. Manatee");
+						int choice = getInt("\nEnter animal species: ", scanner);
+						Animal animal = animalChoice(choice, scanner);
+						if(animal == null) { System.out.println("\n\t\tPlease enter a valid animal species."); continue; }
+						aquarium.addAnimal(animal, generateId(aquarium));
+						break;
+					}
+				
 					break;
 				}
 				case 2 : {
 					aquarium.printAnimals();
+					int id = getInt("\n\tEnter ID of animal to remove: ", scanner);
+					Animal animal = aquarium.removeAnimal(id);
+					if(animal != null) System.out.println("\n\n\t\t\tRemoved: " + animal.getName());
 					break;
 				}
 				case 3 : {
-					System.out.println("\n\n\tExitting...\n\n");
-					return;
+					aquarium.printAnimals();
+					break;
 				}
 				default : {
-					System.out.println("ha gayy");
+					System.out.println("\nPlease enter a number in range.");
+					break;
 				}
 			}
 		}
